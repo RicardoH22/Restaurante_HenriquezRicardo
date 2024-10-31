3e73abdf1fe58399f8966e6eb70c0f7a66a0ffec
rhenriqu <rhenriqu@cuc.edu.co>
2024-10-31 12:07:40 -0500

    front menuscompleto

--- src/app/componentes/menu/MenuCrear.tsx
+++ src/app/componentes/menu/MenuCrear.tsx
@@ -1,8 +1,86 @@
 export const MenuCrear = () => {
   return (
     <>
-      Soy el Menu Crear <br />
-      Menu...
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="cold-md-6">
+          <form className="row g-3 needs-validation">
+            <div className="col-md-6">
+              <label htmlFor="nomb" className="form-label">
+                Nombre Plato
+              </label>
+              <input
+                type="text"
+                className="form-control border-black"
+                id="nomb"
+                name="nomb"
+                required
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+
+            <div className="col-md-6">
+              <label htmlFor="acomp" className="form-label">
+                Acompañamiento
+              </label>
+              <input
+                type="text"
+                className="form-control border-black"
+                id="acomp"
+                name="acomp"
+                required
+              />
+              <div className="valid-feedback">Looks good!</div>
+            </div>
+
+            <div className="col-md-6">
+              <label htmlFor="prec" className="form-label">
+                Precio
+              </label>
+              <input
+                type="text"
+                className="form-control border-black"
+                id="prec"
+                name="prec"
+                required
+              />
+              <div className="invalid-feedback">
+                Please provide a valid city.
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <label htmlFor="regi" className="form-label">
+                Region
+              </label>
+              <select
+                className="form-select border-black"
+                id="regi"
+                name="regi"
+                required
+              >
+                <option selected disabled value="">
+                  Seleccione la Region
+                </option>
+                <option>Caribe</option>
+                <option>Andina</option>
+                <option>Pacifica</option>
+                <option>Amazonia</option>
+                <option>Orinoquia</option>
+                <option>Insular</option>
+              </select>
+              <div className="invalid-feedback">
+                Please select a valid state.
+              </div>
+            </div>
+
+            <div className="col-12">
+              <button className="btn btn-primary" type="submit">
+                Registrar Plato
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/componentes/menu/MenuListar.tsx
+++ src/app/componentes/menu/MenuListar.tsx
@@ -1,8 +1,69 @@
 export const MenuListar = () => {
   return (
     <>
-      Soy el Menu Listar <br />
-      Menu...
+      <div className="pt-5 d-flex justify-content-center">
+        <div className="cold-md-6">
+          <table className="table">
+            <thead>
+              <tr>
+                <th style={{ width: "5%" }}> No.</th>
+                <th style={{ width: "30%" }}> Nombre Plato</th>
+                <th style={{ width: "25" }}> Acompañamiento</th>
+                <th style={{ width: "20%" }} className="text-center">
+                  {" "}
+                  Precio
+                </th>
+                <th style={{ width: "20%" }}> Region</th>
+                <th style={{ width: "5%" }}> opciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td> 1</td>
+                <td>bandeja Paisa</td>
+                <td>Con chinchurria</td>
+                <td className="text-center">35000</td>
+                <td>Andina</td>
+                <td>
+                  <a href="/menupdt/1">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </a>
+                  &nbsp;
+                  <i className="fa-solid fa-eraser"></i>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Ajiaco</td>
+                <td>Con arroz de coco</td>
+                <td>25000</td>
+                <td>Caribe</td>
+                <td>
+                  <a href="/menupdt/2">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </a>
+                  &nbsp;
+                  <i className="fa-solid fa-eraser"></i>
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Mote de queso </td>
+                <td>Con arroz blanco</td>
+                <td>30000</td>
+                <td>Amazonia</td>
+                <td>
+                  <a href="/menupdt/3">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </a>
+                  &nbsp;
+                  <i className="fa-solid fa-eraser"></i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
